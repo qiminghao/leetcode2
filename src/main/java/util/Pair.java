0dead4fe13e8ca4b828e04a822dbf9d8c7f18f72
qiminghao <qiminghao@zju.edu.cn>
2019-12-28 05:14:35 +0800

    new class

---
+++ src/main/java/util/Pair.java
@@ -0,0 +1,45 @@
+package util;
+
+import java.util.Objects;
+
+public class Pair<K, V> {
+
+    private final K element0;
+    private final V element1;
+
+    public static <K, V> Pair<K, V> createPair(K element0, V element1) {
+        return new Pair<>(element0, element1);
+    }
+
+    public Pair(K element0, V element1) {
+        this.element0 = element0;
+        this.element1 = element1;
+    }
+
+    public K getElement0() {
+        return element0;
+    }
+
+    public V getElement1() {
+        return element1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return element0.equals(pair.element0) &&
+                element1.equals(pair.element1);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(element0, element1);
+    }
+
+}
