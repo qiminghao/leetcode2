15058e88a2a8a579cd93b373a92c485f16939a79
qiminghao <qiminghaox@gmail.com>
2020-01-09 04:51:13 +0800

    304. Range Sum Query 2D - Immutable

---
+++ src/main/java/util/NumMatrix.java
@@ -0,0 +1,40 @@
+package util;
+
+/**
+ * @author qiminghao
+ * @version 1.0.0
+ * @ClassName NumMatrix.java
+ * @Description 304. Range Sum Query 2D - Immutable
+ * @createTime 2020/1/9 4:34
+ */
+public class NumMatrix {
+
+    private int[][] sum = null;
+
+    public NumMatrix(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+        int m = matrix.length, n = matrix[0].length;
+        this.sum = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = matrix[i - 1][j - 1] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        row1++;
+        col1++;
+        row2++;
+        col2++;
+        return sum[row2][col2] - sum[row1 - 1][col2] - sum[row2][col1 - 1] + sum[row1 - 1][col1 - 1];
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
