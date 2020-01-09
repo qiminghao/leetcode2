e4db8c7320996fe357d0a743ebc766f783d1ba3f
qiminghao <qiminghaox@gmail.com>
2020-01-10 02:30:13 +0800

    145. Binary Tree Postorder Traversal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -712,6 +712,27 @@ public class Main {
 //        System.out.println(decodeString("2[abc]3[cd]ef"));
     }
 
+    // 145. Binary Tree Postorder Traversal
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            res.add(0, cur.val);
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+        }
+        return res;
+    }
+
     // 1048. Longest String Chain
     public int longestStrChain(String[] words) {
         if (words == null || words.length == 0) {
