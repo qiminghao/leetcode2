9b5b226af1c34f91ac24c79348f34f9ef901243b
qiminghao <qiminghaox@gmail.com>
2020-01-11 02:29:54 +0800

    199. Binary Tree Right Side View

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -712,6 +712,32 @@ public class Main {
 //        System.out.println(decodeString("2[abc]3[cd]ef"));
     }
 
+    // 199. Binary Tree Right Side View
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.remove();
+                if (i == size - 1) {
+                    res.add(cur.val);
+                }
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+        }
+        return res;
+    }
+
     // 515. Find Largest Value in Each Tree Row
     public List<Integer> largestValues(TreeNode root) {
         List<Integer> res = new ArrayList<>();
